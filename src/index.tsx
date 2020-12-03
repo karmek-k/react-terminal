0c59543e57d014beffad62cd70a79b7901b4354f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-03 16:31:38 +0100

    Add example text to main component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import styles from './main.module.css';
+
 export const ReactTerminal = () => {
-  return <p>Hello world!</p>;
+  return (
+    <div className={styles.terminal}>
+      <p>Hello world!</p>
+      <p>test test test</p>
+      <p>test test test test test</p>
+    </div>
+  );
 };
