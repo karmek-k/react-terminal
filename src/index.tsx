761f2044f45413ba2262335bacc5b37ada73ef6e
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-10 21:51:17 +0100

    Suppress an unwanted warning

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Motd from './components/Motd';
 import TerminalLine from './components/TerminalLine';
 import styles from './main.module.css';
+
+// for some reason eslint complains about TerminalSettings not being used
+// eslint-disable-next-line no-unused-vars
 import TerminalContext, { TerminalSettings } from './TerminalContext';
 
 export const ReactTerminal = (props: TerminalSettings) => {
